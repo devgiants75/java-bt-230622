877cffed8db5f69a429a7d24d4dc49b6bf1de6f5
devgiants75 <devgiants75@naver.com>
2023-07-07 11:44:23 +0900

    20230707 Z_Practice02 code upload

--- src/Chapter05/Inheritance/Z_Practice02.java
+++ src/Chapter05/Inheritance/Z_Practice02.java
@@ -9,10 +9,51 @@ package Chapter05.Inheritance;
 // , 생성자를 통해 초기화합니다. 
 // 'fileSize'에 대한 getter 메소드를 작성하세요.
 
+class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+class Ebook extends Book {
+    private double fileSize;
+
+    public Ebook(String title, String author, double price, double fileSize) {
+        super(title, author, price);
+        this.fileSize = fileSize;
+    }
+
+    public double getFileSize() {
+        return fileSize;
+    }
+}
+
 // 테스트를 위해 'Ebook' 클래스의 객체를 하나 생성하고, 모든 필드의 값을 출력하세요.
 public class Z_Practice02 {
 	public static void main(String[] args) {
-		
+		Ebook ebook = new Ebook("Java Programming", "Author Name", 19.99, 1.5);
+        System.out.println("Title: " + ebook.getTitle());
+        System.out.println("Author: " + ebook.getAuthor());
+        System.out.println("Price: " + ebook.getPrice());
+        System.out.println("File Size: " + ebook.getFileSize() + "MB");
 	}
 
 }
