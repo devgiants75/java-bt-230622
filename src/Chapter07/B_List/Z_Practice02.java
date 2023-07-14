6083b3165d8810ea6dac86a40ecc2a41c2e7745f
devgiants75 <devgiants75@naver.com>
2023-07-14 11:48:21 +0900

    20230714

--- src/Chapter07/B_List/Z_Practice02.java
+++ src/Chapter07/B_List/Z_Practice02.java
@@ -1,5 +1,34 @@
 package Chapter07.B_List;
 
-public class Z_Practice02 {
+//Student 클래스를 작성하고, 다음 멤버 변수를 포함하게 하세요
+// : id (정수형), name (문자열), grade (정수형). 
 
+// 생성자: id (정수형), name (문자열), grade (정수형).
+// getter: id (정수형), name (문자열), grade (정수형).
+// setter: grade (정수형). 
+
+//Student 객체를 여러 개 포함하는 ArrayList<Student>를 생성하세요.
+//몇몇 학생 객체를 추가하고, 모든 학생 정보를 출력하는 메서드를 작성하세요.
+
+//	id가 일치하는 학생을 찾아서 성적을 변경하고
+//  , 변경된 학생 정보를 출력하는 메서드를 작성하세요.
+//	grade가 특정 점수 이상인 학생의 목록을 출력하는 메서드를 작성하세요.
+
+// printStudents 메서드: List를 순회(List<Student> students)
+
+// updateGrade 메서드(List<Student> students, int id, int newGrade)
+// : List를 순회 
+// + if조건문(getter로 받아온 id값이, 매개변수의 id값과 같다면 setter메서드를 사용해서 grade값을 변경)
+
+// printHighGradeStudents 메서드(List<Student> students, int minGrade)
+// : List를 순회
+// + if조건문(getter로 받아온 grade값이 minGrade보다 같거나 클 경우 해당 student값을 출력)
+
+class Product {
+	
+}
+
+public class Z_Practice02 {
+	public static void main(String[] args) {
+	}
 }
---
+++ src/Chapter07/B_List/Z_Practice02_Code.java
@@ -0,0 +1,7 @@
+package Chapter07.B_List;
+
+public class Z_Practice02_Code {
+	public static void main(String[] args) {
+		
+	}
+}
